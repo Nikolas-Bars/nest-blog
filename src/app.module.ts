ce65c36df91910ae5e090a399e11794616e2cbd3
Nikolas-Bars~ <kasp2409@mail.ru>
2024-05-08 19:46:22 +0300

    start

---
+++ src/app.module.ts
@@ -0,0 +1,64 @@
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  Provider,
+} from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { appSettings } from './settings/app-settings';
+import { UsersRepository } from './features/users/infrastructure/users.repository';
+import { UsersService } from './features/users/application/users.service';
+import { UsersQueryRepository } from './features/users/infrastructure/users.query-repository';
+import { User, UserSchema } from './features/users/domain/user.entity';
+import { UsersController } from './features/users/api/users.controller';
+import { LoggerMiddleware } from './common/middlewares/logger.middleware';
+import { NameIsExistConstraint } from './common/decorators/validate/name-is-exist.decorator';
+
+const usersProviders: Provider[] = [
+  UsersRepository,
+  UsersService,
+  UsersQueryRepository,
+];
+
+@Module({
+  // Регистрация модулей
+  imports: [
+    MongooseModule.forRoot(
+      appSettings.env.isTesting()
+        ? appSettings.api.MONGO_CONNECTION_URI_FOR_TESTS
+        : appSettings.api.MONGO_CONNECTION_URI,
+      { dbName: 'blogsdb' }
+    ),
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+  ],
+  // Регистрация провайдеров
+  providers: [
+    ...usersProviders,
+    NameIsExistConstraint,
+    /* {
+            provide: UsersService,
+            useClass: UsersService,
+        },*/
+    /*{
+            provide: UsersService,
+            useValue: {method: () => {}},
+
+        },*/
+    // Регистрация с помощью useFactory (необходимы зависимости из ioc, подбор провайдера, ...)
+    /* {
+            provide: UsersService,
+            useFactory: (repo: UsersRepository) => {
+                return new UsersService(repo);
+            },
+            inject: [UsersRepository]
+        }*/
+  ],
+  // Регистрация контроллеров
+  controllers: [UsersController],
+})
+export class AppModule implements NestModule {
+  // https://docs.nestjs.com/middleware#applying-middleware
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
---
+++ src/features/users/domain/user.entity.ts
@@ -0,0 +1,44 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument, Model } from 'mongoose';
+import { randomUUID } from 'crypto';
+
+export type UserDocument = HydratedDocument<User>;
+
+export type UserModelType = Model<UserDocument> & typeof statics;
+
+@Schema()
+export class User {
+  @Prop()
+  name: string;
+
+  @Prop()
+  email: string;
+
+  @Prop()
+  createdAt: Date;
+
+  updateUser(name: string) {
+    this.name = name;
+  }
+
+  static createUser(name: string, email: string | null) {
+    const user = new this();
+
+    user.name = name;
+    user.email = email ?? `${randomUUID()}_${name}@it-incubator.io`;
+
+    return user;
+  }
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
+
+UserSchema.methods = {
+  updateUser: User.prototype.updateUser,
+};
+
+const statics = {
+  createUser: User.createUser,
+};
+
+UserSchema.statics = statics;
